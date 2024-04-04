4358ec91ae5333c5a593c39932a2d2ea545cc76e
Cristiano <cristianofran@hotmail.com>
2024-04-04 13:16:17 -0300

    Adição do delegate func com parâmetro.

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DelegateUsandoParametro();
+
+            Console.ReadKey();
+        }
+
+        private static void DelegateUsandoParametro()
+        {
+            Console.Write("Digite seu nome: ");
+            var nome = Console.ReadLine();
+
+            Func<string, string> Saudacao = BomDia;
+            var saudacao = Saudacao(nome);
+
+            Console.WriteLine(saudacao);
+        }
+
+        private static string BomDia(string nome) 
+            => $"Bom dia, {nome}";
+    }
+}
