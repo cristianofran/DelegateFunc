9a101ad87b95b41c815afc790a1e317ab9d82c76
Cristiano <cristianofran@hotmail.com>
2024-04-04 13:33:16 -0300

    Adição do delegate func com dois parametros.

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -5,9 +5,23 @@
         static void Main()
         {
             DelegateUsandoParametro();
-
+            DelegateUsandoDoisParametros();
             Console.ReadKey();
         }
+
+        private static void DelegateUsandoDoisParametros()
+        {
+            var parcela1 = 9.7;
+            var parcela2 = 0.3;
+
+            Func<double, double, double> Calculadora = Somar;
+            var soma = Calculadora(parcela1, parcela2);
+
+            Console.WriteLine("{0} + {1} = {2}", parcela1, parcela2, soma);
+        }
+
+        private static double Somar(double parcela1, double parcela2)
+            => parcela1 + parcela2;
 
         private static void DelegateUsandoParametro()
         {
